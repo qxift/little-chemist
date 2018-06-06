bac3bf541cc2cb363722211f382b4f25fee1df64
Margo Novikov <Margo@macs-MacBook-Pro-2.local>
2018-06-06 16:17:58 -0400

    new images

--- src/IconGUI.java
+++ src/IconGUI.java
@@ -10,12 +10,12 @@ public class IconGUI extends JLabel {
 	public IconGUI(Element other)
 	{
 		super(new ImageIcon("../little-chemist/images/" + other.name + ".jpg"));
-//		icon = new ImageIcon("../little-chemist/images/" + other.name + ".jpg");
 		setMinimumSize(new Dimension(50,50));
-		setMaximumSize(new Dimension(100,50));
+		setMaximumSize(new Dimension(100,100));
 //		setPreferredSize(new Dimension(50,50));
 		element = other;
 		//setText(element.name);	
+		//setIcon(new ImageIcon("../little-chemist/images/" + other.name + ".jpg"));
 	}
 	
 	public Element getElement()
